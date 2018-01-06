e34b796f08755bdd8352d1c0037cd84fec2aed66
Egor Spiriadi <drovosek01tnt@gmail.com>
2018-01-06 15:47:28 +0300

    Complete Lab2

---
+++ Lab3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDeque<int> deq = new MyDeque<int>();
+            deq.EnqueueFirst(1);
+            deq.EnqueueFirst(2);
+            deq.EnqueueFirst(3);
+            deq.EnqueueFirst(4);
+            deq.EnqueueFirst(5);
+            deq.EnqueueLast(6);
+            deq.EnqueueLast(7);
+            deq.EnqueueLast(8);
+            deq.EnqueueLast(9);
+            deq.EnqueueLast(10);
+
+            bool check = deq.Contains(5);
+            var head = deq.PeekFirst();
+            var tail = deq.PeekLast();
+
+            //deq.Clear();
+
+            deq.DequeueFirst();
+            deq.DequeueFirst();
+            deq.DequeueFirst();
+            deq.DequeueFirst();
+            deq.DequeueFirst();
+            deq.DequeueLast();
+            deq.DequeueLast();
+            deq.DequeueLast();
+            deq.DequeueLast();
+            deq.DequeueLast();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab3")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b02a27f2-4874-4f22-a5d4-3baa942c49bc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
